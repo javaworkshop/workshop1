c7bf38ac8454f9f32a8034ff490c8ffdc7a40077
gerbrich2 <gerbrich2@10.17.190.17>
2015-08-10 15:26:18 +0200

    BestellingGenerator

---
+++ Workshop1/src/model/BestellingSQL.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author gerbrich2
+ */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class BestellingSQL {
+	
+	static Connection connection; //initialize this
+
+	public static void addToDatabase(Bestelling bestelling){
+			try{
+				//zorg ervoor dat alle datafields van de bestelling naar de database worden geschreven
+
+				
+				String sql = "INSERT INTO Bestelling (klant_id), VALUES (?)";
+				PreparedStatement statement = connection.prepareStatement(sql);
+				statement.setInt(1, bestelling.getKlantId());
+				int rowsInserted = statement.executeUpdate();
+				if (rowsInserted>0){
+					System.out.println("Bestelling toegevoegd");
+				}
+			}
+			catch(SQLException ex){
+				//make exception pop up
+			}
+			catch (Exception ex){
+				//make exception pop up
+			}
+	}
+}
+

f0bb46382caf179ce991a3460a962bdd9e8c36a6
gerbrich2 <gerbrich2@macbook-pro-2.fritz.box>
2015-08-23 17:28:57 +0200

    Methode buildInsertStatementKlant gemaakt die gebruik maakt van annotation, en annotation klasse onderaan het document toegevoegd

--- Workshop1/src/database/SqlCodeGenerator.java
+++ Workshop1/src/database/SqlCodeGenerator.java
@@ -1,7 +1,14 @@
 
 package database;
 
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 import model.Artikel;
 import model.Bestelling;
 import model.Klant;
@@ -20,8 +27,45 @@ class SqlCodeGenerator {
      * @return      sql code to insert klant
      */
     
+    //methode die gebruik maakt van annotation
+    public String buildAnnotationInsertStatement(Object object){
+        int variableToInsert = 0;
+        String sqlTableName = object.getClass().getSimpleName().toUpperCase();
+        String buildSqlStatement = "INSERT INTO " + sqlTableName + "("; 
+        String valueFieldEnd = "values (";
+        Field[] declaredFields = object.getClass().getDeclaredFields();
+        
+           for (Field field : declaredFields){
+            try{
+                    field.setAccessible(true);
+                    if (field.get(object) != null && !isPrimitiveZero(field.get(object))){
+                        ArrayList<Annotation> annotations = (ArrayList<Annotation>)Arrays.asList(field.getDeclaredAnnotations());
+                        //misschien ook iets anders doen met annotatie @id
+                        if (annotations.contains(Column.class)|| annotations.contains(Id.class)){
+                            variableToInsert++;
+                            if (variableToInsert > 1){
+                                buildSqlStatement += ", ";
+                                valueFieldEnd += ", ";
+                            }
+                            buildSqlStatement += field.getName();
+                            if (field.get(object) instanceof String) {
+                                valueFieldEnd += "\'" + field.get(object) + "\'";
+                            }
+                            else{
+                                valueFieldEnd += field.get(object);
+                            } 
+                        }
+                    }
+            }
+            catch (IllegalArgumentException | IllegalAccessException | SecurityException e){
+                e.printStackTrace();
+            }
+        }
+        return buildSqlStatement + ") " + valueFieldEnd + ")";
+    }
+    
     //methode die gebruikt maakt van reflection en alle typen objecten accepteert
-    public String buildInsertStatementKlant(Object object){
+    public String buildInsertStatement(Object object){
         int variableToInsert = 0;
         String sqlTableName = object.getClass().getSimpleName().toUpperCase();
         String buildSqlStatement = "INSERT INTO " + sqlTableName + "("; 
@@ -331,3 +375,12 @@ class SqlCodeGenerator {
         }
     
 }
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface Column{}
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface Id{}
+

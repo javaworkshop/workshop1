297ccf9f5a223e1d9e134737c00714e981b41442
gerbrich2 <gerbrich2@macbook-pro.fritz.box>
2015-08-26 20:01:29 +0200

    column annotation with parameter

--- Workshop1/src/model/Bestelling.java
+++ Workshop1/src/model/Bestelling.java
@@ -3,6 +3,7 @@ package model;
 import database.Column;
 import database.Entity;
 import database.Id;
+import database.Table;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.IntegerProperty;
@@ -13,21 +14,22 @@ import javafx.beans.property.SimpleStringProperty;
 /**
  * Class to store data from the bestelling table.
  */
-@Entity
+@Entity()
+@Table(name = "bestellingen")
 public class Bestelling extends Data {
-    @Id private final SimpleIntegerProperty klant_id; // De klant die de bestelling gedaan heeft
-    @Column private final SimpleIntegerProperty artikel_id1;
-    @Column private final SimpleIntegerProperty artikel_id2;
-    @Column private final SimpleIntegerProperty artikel_id3;
-    @Column private final SimpleStringProperty artikel_naam1;
-    @Column private final SimpleStringProperty artikel_naam2;
-    @Column private final SimpleStringProperty artikel_naam3;
-    @Column private final SimpleIntegerProperty artikel_aantal1;
-    @Column private final SimpleIntegerProperty artikel_aantal2;
-    @Column private final SimpleIntegerProperty artikel_aantal3;
-    @Column private final SimpleDoubleProperty artikel_prijs1;
-    @Column private final SimpleDoubleProperty artikel_prijs2;
-    @Column private final SimpleDoubleProperty artikel_prijs3;
+    @Column(name = "klant_id") private final SimpleIntegerProperty klant_id; // De klant die de bestelling gedaan heeft
+    @Column(name = "artikel_id1") private final SimpleIntegerProperty artikel_id1;
+    @Column(name = "artikel_id2") private final SimpleIntegerProperty artikel_id2;
+    @Column(name = "artikel_id3") private final SimpleIntegerProperty artikel_id3;
+    @Column(name = "artikel_naam1") private final SimpleStringProperty artikel_naam1;
+    @Column(name = "artikel_naam2") private final SimpleStringProperty artikel_naam2;
+    @Column(name = "artikel_naam3") private final SimpleStringProperty artikel_naam3;
+    @Column(name = "artikel_aantal1") private final SimpleIntegerProperty artikel_aantal1;
+    @Column(name = "artikel_aantal2") private final SimpleIntegerProperty artikel_aantal2;
+    @Column(name = "artikel_aantal3") private final SimpleIntegerProperty artikel_aantal3;
+    @Column(name = "artikel_prijs1") private final SimpleDoubleProperty artikel_prijs1;
+    @Column(name = "artikel_prijs2") private final SimpleDoubleProperty artikel_prijs2;
+    @Column(name = "artikel_prijs3") private final SimpleDoubleProperty artikel_prijs3;
     
     public Bestelling() {
         klant_id = new SimpleIntegerProperty(0);

1e1e6cc7949823eee3692f992b6017babf0bc107
gerbrich2 <gerbrich2@macbook-pro.fritz.box>
2015-08-26 20:03:00 +0200

    Id annotation with name parameter

--- Workshop1/src/database/Id.java
+++ Workshop1/src/database/Id.java
@@ -16,4 +16,6 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Id{}
+public @interface Id{
+String name() default "";
+}
